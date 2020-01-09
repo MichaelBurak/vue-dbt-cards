a43e7d2be7e1ff7032fb7ffbaf7c6c3e94615842
MichaelBurak <michaelrburak@gmail.com>
2020-01-08 18:13:10 -0700

    accept and close closes all modals

--- src/components/Radical.vue
+++ src/components/Radical.vue
@@ -48,15 +48,7 @@
     </v-dialog>
     <v-row>
       <v-dialog v-model="difficultyModal" persistent max-width="290">
-        <template v-slot:activator="{ on }">
-          <!-- <v-list-item>
-            <v-list-item-content>
-              <v-row>
-                <v-btn color="primary" dark v-on="on">Radical Acceptance</v-btn>
-              </v-row>
-            </v-list-item-content>
-          </v-list-item>-->
-        </template>
+        <template v-slot:activator="{ on }"></template>
         <v-card>
           <v-card-title class="headline">Radical Acceptance</v-card-title>
           <v-card-text>What is the difficult part of the situation you are trying to accept?</v-card-text>
@@ -72,15 +64,7 @@
     </v-row>
     <v-row>
       <v-dialog v-model="finalModal" persistent max-width="290">
-        <template v-slot:activator="{ on }">
-          <!-- <v-list-item>
-            <v-list-item-content>
-              <v-row>
-                <v-btn color="primary" dark v-on="on">Radical Acceptance</v-btn>
-              </v-row>
-            </v-list-item-content>
-          </v-list-item>-->
-        </template>
+        <template v-slot:activator="{ on }"></template>
         <v-card>
           <v-card-title class="headline">Radical Acceptance</v-card-title>
           <v-card-text>What is the reality of the situation you are trying to accept?</v-card-text>
@@ -88,8 +72,12 @@
           <v-textarea outlined name="input-7-4" label="Reality" />
           <v-card-actions>
             <v-spacer></v-spacer>
-            <!-- <v-btn text @click="finalModal = true">Next Step</v-btn> -->
-            <v-btn color="green darken-1" text @click="finalModal = false">Accept and Close</v-btn>
+
+            <v-btn
+              color="green darken-1"
+              text
+              @click="finalModal = false,difficultyModal=false,rModal=false "
+            >Accept and Close</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
